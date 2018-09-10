adef7b1abf769ebc22c85592a652100c4a41d502
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-09-10 10:41:26 -0400

    Revert "Output AWS region and VPC ID"
    This reverts commit 5cc81423c5ac8b914206ce67164570f59cb2f841.

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "public_ip"   {
 output "public_fqdn" {
   value = "${aws_route53_record.openvpn.fqdn}"
 }
-
-output "aws_region" {
-  value = "${var.aws_region}"
-}
-
-output "vpc_id" {
-  value = "${aws_vpc.openvpn.id}"
-}
